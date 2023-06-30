cdfabcb942df5ddf38aab77187fddd9cdc2cb168
creating2morrow <creating_tomorrow@proton.me>
2023-06-30 05:06:36 -0400

    tag release v0.4.5-alpha

--- neveko-gui/src/main.rs
+++ neveko-gui/src/main.rs
@@ -39,7 +39,7 @@ fn main() -> Result<(), eframe::Error> {
         })
     });
     eframe::run_native(
-        "neveko-gui-v0.4.4-alpha",
+        "neveko-gui-v0.4.5-alpha",
         options,
         Box::new(|cc| Box::new(neveko_gui::WrapApp::new(cc))),
     )
