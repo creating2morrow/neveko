696d1ad300d83bf30d26c080257442e279d33b4a
creating2morrow <creating_tomorrow@proton.me>
2024-04-10 00:33:41 -0400

    v0.1.1-beta

--- neveko-gui/src/main.rs
+++ neveko-gui/src/main.rs
@@ -39,7 +39,7 @@ fn main() -> Result<(), eframe::Error> {
         })
     });
     eframe::run_native(
-        "neveko-gui-v0.1.0-beta",
+        "neveko-gui-v0.1.1-beta",
         options,
         Box::new(|cc| Box::new(neveko_gui::WrapApp::new(cc))),
     )
